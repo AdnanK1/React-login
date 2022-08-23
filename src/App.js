bd944c4a382ff95cfda04a0613169bc78f2cb7d1
AdnanK1 <adnangitonga@gmail.com>
2022-08-23 22:31:17 +0300

    Google Identity Services Login with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import jwtDecode from "jwt-decode";
 
 function App() {
+  const [user, setUser] = useState({});
+
+  function handleCallBackResponse(res) {
+    console.log("Encoded JWT ID Token:" + res.credential);
+    var userObject = jwtDecode(res.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signDiv").hidden = true;
+  }
+
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id:
+        "926521009509-iefldr67466nd5eumbrq6sajk9db2iod.apps.googleusercontent.com",
+      callback: handleCallBackResponse,
+    });
+    google.accounts.id.renderButton(document.getElementById("signDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+    google.accounts.id.prompt();
+  }, []);
+  // If we have no user show sign in button
+  // If we have a user show logout button
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signDiv"></div>
+      {Object.keys(user).length !== 0 && (
+        <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
+      )}
+
+      {user && (
+        <div>
+          <img src={user.picture}></img>
+          <h3> {user.name} </h3>
+        </div>
+      )}
     </div>
   );
 }
